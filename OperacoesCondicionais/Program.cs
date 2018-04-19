a6dfb6dbb5f4158a624461d1328aff4f65f7d3eb
LUCAS ALBERTO FERREIRA MIRANDA <lucas.afmiranda@senacsp.edu.br>
2018-04-19 11:11:09 -0300

    Declaração de variaveis

--- OperacoesCondicionais/Program.cs
+++ OperacoesCondicionais/Program.cs
@@ -6,7 +6,16 @@ namespace OperacoesCondicionais
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Comentario in-line(em linha)
+            /*
+             Comentario em in-block(em bloco)
+            */
+
+            //declaração da variavel operação. Ela será do tipo inteiro
+            int operação;
+
+            //declaração das variaveis n1 e n2. Elas serão do tipo double
+            double n1, n2;
         }
     }
 }
